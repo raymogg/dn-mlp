1cb7b086a63f193b759c094be7e5c5cc78ff0a62
raymogg <raymondmogg@gmail.com>
2022-10-25 17:14:56 +1000

    initial exposure calcs

--- helpers.py
+++ helpers.py
@@ -2,6 +2,7 @@
 # simply read from contract
 from fractions import Fraction
 
+# Returns the addresses of all assets currently in MLP
 def getMLPTokens(vault):
     num_tokens = vault.functions.allWhitelistedTokensLength().call()
     tokens = []
@@ -10,6 +11,7 @@ def getMLPTokens(vault):
         tokens.append(token)
     return tokens
 
+# Returns the target weights of every asset in the tokens list
 def getTokensTargetWeights(vault, tokens):
     weights = []
     total_weights = vault.functions.totalTokenWeights().call()
@@ -19,22 +21,24 @@ def getTokensTargetWeights(vault, tokens):
         weights.append([token, weight])
     return weights
 
+# Returns the target weight of a given token
 def getTokenTargetWeight(vault, token):
     target = vault.functions.tokenWeights(token).call()
     total = vault.functions.totalTokenWeights().call()
     return Fraction(target, total)
 
-# get token current weights (USD value)
-# get token USD value and div by USD supply
+# Returns the token price reported by the MYC oracle for a given token
 def getTokenPrice(vault, token):
     # todo this seems wrong - this should probably call the oracle of the token
     price = vault.functions.getMaxPrice(token).call()
     return price
 
+# Returns the token weight for agiven token
 def getTokenCurrentWeight(vault, token):
     # todo return the tokens USDG weight
     print("getTokenCurrentWeight")
 
+# Returns the token weights of every asset in the tokens list
 def getTokensCurrentWeights(vault, tokens):
     total = 0
     raw_amounts = []
@@ -47,10 +51,28 @@ def getTokensCurrentWeights(vault, tokens):
         target_weights.append([raw[0], Fraction(raw[1], total)])
     return target_weights
 
+# Returns the amount of tokens held by the MLP vault for each token in the vault
+def getTokensUSDGAmounts(vault, tokens):
+    amounts = []
+    for token in tokens:
+        amount = getTokenUSDGAmount(vault, token)
+        amounts.append([token, amount])
+    return amounts
+
+# gets the amount of tokens held by the MLP vault denominated in USDG (USD)
 def getTokenUSDGAmount(vault, token):
     usdg_amount = vault.functions.usdgAmounts(token).call()
     return usdg_amount
 
-
-
-# calculate hedge amount for asset
+# Compute USD exposure to each asset for a given amount of mlp
+def usdExposure(vault, tokens, mlp, mlp_balance):
+    # compute share of MLP
+    mlp_supply = mlp.functions.totalSupply().call()
+    share = Fraction(mlp_balance, mlp_supply)
+    # get amount of each asset held by vault denominated in USDG
+    token_amounts = getTokensUSDGAmounts(vault, tokens)
+    token_shares = []
+    # for each token compute net USDG exposure for holding mlp_balance units of MLP.
+    for token_amount_pair in token_amounts:
+        token_shares.append([token_amount_pair[0], token_amount_pair[1] * share])
+    return token_shares
--- main.py
+++ main.py
@@ -16,16 +16,25 @@ vault_address = os.getenv("VAULT")
 vault_file = open("./contracts/Vault.json")
 vault_abi = json.load(vault_file)["abi"]
 vault = w3.eth.contract(abi=vault_abi, address=vault_address)
+mlp_address = os.getenv("MLP")
+mlp_file = open("./contracts/MLP.json")
+mlp_abi = json.load(mlp_file)["abi"]
+mlp = w3.eth.contract(abi=mlp_abi, address=mlp_address)
+
 mlp_tokens = helpers.getMLPTokens(vault)
 
 # Vault
 # test get token weights
-token_weights = helpers.getTokensTargetWeights(vault, mlp_tokens)
-print(token_weights)
+# token_weights = helpers.getTokensTargetWeights(vault, mlp_tokens)
+# print(token_weights)
 
-# test get current weights
-target_weights = helpers.getTokensCurrentWeights(vault, mlp_tokens)
-print(target_weights)
+# # test get current weights
+# target_weights = helpers.getTokensCurrentWeights(vault, mlp_tokens)
+# print(target_weights)
+
+total_exposure = helpers.usdExposure(vault, mlp_tokens, mlp, 1000000000000000000)
+print(total_exposure)
+
 # - getMaxPrice / getMinPrice
 # - token balances (exact tokens held by index?)
 
